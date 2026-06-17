edec0cf39fe4cff05cff19e65b2aec1dcbf7ddd3
Santha Aadhira <74529097+aadhira636@users.noreply.github.com>
2026-06-17 11:08:12 +0530

    fourth commit

--- src/Quotegenerator.jsx
+++ src/Quotegenerator.jsx
@@ -1,29 +1,92 @@
+// import React from "react";
+// import Axios from "axios";
+// import { useState, useEffect } from "react";
+
+// const QuoteGenerator = () => {
+//   const [quote, setQuote] = useState("");
+
+//   const apiCallFunc = async () => {
+//     try {
+//       const data = await Axios.get("https://dummyjson.com/quotes/random");
+//       setQuote(data.data.quote);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+    // useEffect(() => {
+    // apiCallFunc();
+    // }, []);
+
+//   return (
+//     <div>
+//       <button onClick={apiCallFunc}>Generate Quote</button>
+//       <h1>{quote}</h1>
+//     </div>
+//   );
+// };
+
+// export default QuoteGenerator;
+
 import React from "react";
 import Axios from "axios";
 import { useState, useEffect } from "react";
 
-const QuoteGenerator = () => {
-  const [quote, setQuote] = useState("");
+const Quotegenrator = () => {
 
-  const apiCallFunc = async () => {
-    try {
-      const data = await Axios.get("https://dummyjson.com/quotes/random");
-      setQuote(data.data.quote);
-    } catch (error) {
-      console.log(error);
+    const [quote, setQuote] = useState("");
+
+    const apiCallFunc = async ()=> {
+        try{
+            const data = await Axios.get("https://dummyjson.com/quotes/random");
+            setQuote(data.data.quote);
+        } catch (error){
+            console.log(error);
+        }
     }
-  };
 
-    useEffect(() => {
-    apiCallFunc();
-    }, []);
+        useEffect(() => {
+          apiCallFunc();
+        }, []);
 
-  return (
-    <div>
-      <button onClick={apiCallFunc}>Generate Quote</button>
-      <h1>{quote}</h1>
-    </div>
-  );
-};
+    return (
+        <div>
+            <button onClick={apiCallFunc}>Generate Quote</button>
+            <h1>{quote}</h1>
+        </div>
+    )
 
-export default QuoteGenerator;
+}
+
+export default Quotegenrator;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
